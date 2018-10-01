5561c753253d16c9d36f405e1534c97d50e4b176
Alex <alex.seefried3@gmail.com>
2018-09-30 19:32:04 -0700

    Added Setting.cs to Models folder.

---
+++ DOOFUS/DOOFUS/Models/Setting.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DOOFUS.Models
+{
+    public class Setting
+    {
+        public int Id { get; set; }
+
+        public string Key { get; set; }
+        
+        public int CustomerId { get; set; }
+
+        public string UserName { get; set; }
+
+        //Can be used once DateTime is defined
+        /*
+        public DateTime StartEffectiveDate { get; set; }
+
+        public DateTime EndEffectiveDate { get; set; }
+
+        public DateTime CreatedTimeStamp { get; set; }
+
+        public DateTime LastModifiedTimeStamp { get; set; }
+        */
+
+        public string LastModifiedBy { get; set; }
+
+        public int LastModifiedById { get; set; }
+    }
+}
+ 
+ 
+ 
