6ca32a2483b0729aab8f318a48d78624c6adc33a
JS <jcdentonunatco@protonmail.com>
2018-10-02 13:14:26 -0700

    Added SettingsController

---
+++ DOOFUS/DOOFUS/Controllers/SettingsController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DOOFUS.Controllers
+{
+    public class SettingsController : Controller
+    {
+        // GET: Settings
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET api/values
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        
+    }
+}
